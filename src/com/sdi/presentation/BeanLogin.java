602341e3bbcf437883a6323a7c7eb6d892deba06
miguelgrc <miguelparo12@hotmail.com>
2017-03-04 20:46:26 +0100

    PropertyNonWritable :D

--- src/com/sdi/presentation/BeanLogin.java
+++ src/com/sdi/presentation/BeanLogin.java
@@ -1,5 +1,49 @@
 package com.sdi.presentation;
 
-public class BeanLogin {
+import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.sdi.business.Services;
+import com.sdi.business.UserService;
+import com.sdi.business.exception.BusinessException;
+import com.sdi.dto.User;
+
+@ManagedBean(name = "login")
+@SessionScoped
+public class BeanLogin implements Serializable {
+	private static final long serialVersionUID = -3194940809153687908L;
+	
+	private User user = new User();
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	public String login(){
+		UserService service;		
+		try {
+			service = Services.getUserService();
+			User u = service.findLoggableUser(user.getLogin(), user.getPassword());
+			if(u != null){
+				//Filtrar si es admin o no. Admin->listarUsuarios / NoAdmin->listadoTareas
+				return "login";
+			}
+			else{
+				//Devolver a la misma página
+				return "error";
+			}
+			
+		} catch (BusinessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "error";
+		}
+	}
 }
