357c422b7e8b7303d41476fcce9bf04ab2587e22
miguelgrc <miguelparo12@hotmail.com>
2017-03-09 16:51:14 +0100

    'Finished' admin's userlist commands

--- src/com/sdi/presentation/UsersBean.java
+++ src/com/sdi/presentation/UsersBean.java
@@ -12,6 +12,7 @@ import org.primefaces.event.SelectEvent;
 import com.sdi.business.AdminService;
 import com.sdi.business.Services;
 import com.sdi.dto.User;
+import com.sdi.dto.types.UserStatus;
 
 @ManagedBean(name="users")
 @SessionScoped
@@ -64,6 +65,8 @@ public class UsersBean {
 		
 	}
 	
+	//TODO: SI SE REGISTRA UN NUEVO USUARIO, NO APARECE EN LA LISTA DEL ADMIN HASTA QUE SE REINICIA LA APLICACION!!!
+	
 	private List<User> filtrarNoAdmin(List<User> usuarios){
 		List<User> noAdminUsers = new ArrayList<User>();
 		for(User user: usuarios){
@@ -78,10 +81,30 @@ public class UsersBean {
 		AdminService service;
 		try {
 			service = Services.getAdminService();
-			if(selected != null)
+			if(selected != null){
 				service.deepDeleteUser(selected.getId());
-			// Actualizamos el javabean de users inyectado en la tabla.
-			users = filtrarNoAdmin(service.findAllUsers());
+				// Actualizamos el javabean de users inyectado en la tabla.
+				users = filtrarNoAdmin(service.findAllUsers());
+			}
+			return "exito";
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
+	
+	public String cambiarEstado() {
+		AdminService service;
+		try {
+			service = Services.getAdminService();
+			if(selected != null){
+				if(selected.getStatus() == UserStatus.ENABLED)
+					service.disableUser(selected.getId());
+				else
+					service.enableUser(selected.getId());
+				// Actualizamos el javabean de users inyectado en la tabla.
+				users = filtrarNoAdmin(service.findAllUsers());
+			}
 			return "exito";
 		} catch (Exception e) {
 			e.printStackTrace();
