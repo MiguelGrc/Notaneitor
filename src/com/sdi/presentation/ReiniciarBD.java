e5403998ce3a58c4d94d9cc86aa2bcfdb35453ee
JorgeZapa <kouuke9@gmail.com>
2017-03-20 17:25:03 +0100

    Acabado reinicio de BD

--- src/com/sdi/presentation/ReiniciarBD.java
+++ src/com/sdi/presentation/ReiniciarBD.java
@@ -16,6 +16,7 @@ import com.sdi.business.UserService;
 import com.sdi.dto.Category;
 import com.sdi.dto.Task;
 import com.sdi.dto.User;
+import com.sdi.persistence.impl.UserDaoJdbcImpl;
 
 @ManagedBean(name = "reiniciar")
 @ApplicationScoped
@@ -36,18 +37,20 @@ public class ReiniciarBD {
 			tServ = Services.getTaskService();
 			uServ = Services.getUserService();
 
+			UserDaoJdbcImpl dUser = new UserDaoJdbcImpl();
+
 			List<User> usuariosActuales = aServ.findAllUsers();
 			for (User u : usuariosActuales) {
-				if(!u.getIsAdmin())
+				if (!u.getIsAdmin())
 					aServ.deepDeleteUser(u.getId());
 			}
 
 			List<User> usuariosNuevos = new ArrayList<User>();
 			for (int i = 1; i <= 3; i++) {
 				User usuario = new User();
-				uServ.registerUser(usuario.setEmail("user" + i + "@gmail.com")
+				dUser.save(usuario.setEmail("user" + i + "@gmail.com")
 						.setId(new Long(i)).setLogin("user" + i)
-						.setPassword("usuario" + i));
+						.setPassword("user" + i));
 				usuariosNuevos.add(usuario);
 			}
 
@@ -56,54 +59,70 @@ public class ReiniciarBD {
 			// .setLogin("user1").setPassword("user1");
 			// uServ.registerUser(usuario1);
 			List<Category> categNuevas = new ArrayList<Category>();
+			usuariosNuevos = aServ.findAllUsers();
+			// for(User u: usuariosNuevos){
+			// if(u.getIsAdmin()){
+			// usuariosNuevos.remove(u);
+			// }
+			// }
+
 			int j = 1;
 			for (User u : usuariosNuevos) {
-				for (int i = 1; i <= 3; i++) {
-					Category categ = new Category();
-					tServ.createCategory(categ.setId(new Long(j))
-							.setUserId(u.getId()).setName("Category" + j * i));
-					categNuevas.add(categ);
-					j++;
+				if (!u.getIsAdmin()) {
+					for (int i = 1; i <= 3; i++) {
+						Category categ = new Category();
+						tServ.createCategory(categ.setId(new Long(j))
+								.setUserId(u.getId())
+								.setName("Category" + j));
+						categNuevas.add(categ);
+						j++;
+					}
 				}
 			}
 
 			j = 1;
 			for (User u : usuariosNuevos) {
-				Task tarea = new Task();
-				List<Category> categoriasUsuario = tServ
-						.findCategoriesByUserId(u.getId());
-				for (int i = 1; i <= 10; i++) {
-					tServ.createTask(tarea
-							.setPlanned(DateUtil.addDays(DateUtil.today(), 6))
-							.setId(new Long(j)).setTitle("Tarea" + j)
-							.setUserId(u.getId()));
-					j++;
-				}
-				for (int i = 1; i <= 10; i++) {
-					tServ.createTask(tarea.setPlanned(DateUtil.today())
-							.setId(new Long(j)).setTitle("Tarea" + j)
-							.setUserId(u.getId()));
-					j++;
-				}
-				for (int i = 1; i <= 10; i++) {
-					Category categ = null;
-					if (i <= 3) {
-						categ = categoriasUsuario.get(1);
-					} else {
-						if (i <= 6) {
-							categ = categoriasUsuario.get(2);
-						} else {
-							if (i <= 10) {
-								categ = categoriasUsuario.get(3);
-							}
-						}
+				if (!u.getIsAdmin()) {
+					Task tarea = new Task();
+					List<Category> categoriasUsuario = tServ
+							.findCategoriesByUserId(u.getId());
+					for (int i = 1; i <= 10; i++) {
+						tServ.createTask(tarea
+								.setPlanned(
+										DateUtil.addDays(DateUtil.today(), 6))
+								.setId(new Long(j)).setTitle("Tarea" + j)
+								.setUserId(u.getId()));
+						j++;
 					}
+					for (int i = 1; i <= 10; i++) {
+						tServ.createTask(tarea.setPlanned(DateUtil.today())
+								.setId(new Long(j)).setTitle("Tarea" + j)
+								.setUserId(u.getId()));
+						j++;
+					}
+					for (int i = 1; i <= 10; i++) {
+						Category categ = null;
+						if (i <= 3) {
+							categ = categoriasUsuario.get(0);
+						} else {
+							if (i <= 6) {
+								categ = categoriasUsuario.get(1);
+							} else {
+								if (i <= 10) {
+									categ = categoriasUsuario.get(2);
+								}
+							}
 
-					tServ.createTask(tarea
-							.setPlanned(DateUtil.addDays(DateUtil.today(), -j))
-							.setId(new Long(j)).setTitle("Tarea" + j)
-							.setUserId(u.getId()).setCategoryId(categ.getId()));
-					j++;
+						}
+
+						tServ.createTask(tarea
+								.setPlanned(
+										DateUtil.addDays(DateUtil.today(), -j))
+								.setId(new Long(j)).setTitle("Tarea" + j)
+								.setUserId(u.getId())
+								.setCategoryId(categ.getId()));
+						j++;
+					}
 				}
 			}
 
